2bf0bcbb8d0ea4b6cbbaef1b0969a7d01e546862
TartunovVA <Vitaliy.Tartunov@GMail.com>
2017-05-25 18:54:24 +0700

    Solved "MaxProductOfThree"

---
+++ CoditySolutions/06 - Sorting/MaxProductOfThree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CoditySolutions
+{
+    [TestFixture]
+    public class MaxProductOfThree
+    {
+        // 100/100
+        public int solution(int[] A)
+        {
+            if (A.Length < 3) return 0;
+
+            Array.Sort(A);
+
+            var last = A.Length-1;
+
+            var max1 = A[last] * A[last - 1] * A[last - 2];
+            var max2 = A[last] * A[0] * A[1];
+
+            return Math.Max(max1, max2);
+        }
+
+        [TestCase(new[] { 1, 2, 3, 20, 10, 1, 10 }, 2000)]
+        [TestCase(new[] { 1, 1, 1, 1, 1, 1, 1 }, 1)]
+        [TestCase(new[] { -1, -2, -3, -20, -10, -1, -10 }, -2)]
+        public void Tests(int[] input, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, solution(input));
+        }
+    }
+}
