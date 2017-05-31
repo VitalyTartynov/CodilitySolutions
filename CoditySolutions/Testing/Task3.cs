92ed004d47066779f617a3f712e90e208e2ddb9f
TartunovVA <Vitaliy.Tartunov@GMail.com>
2017-05-31 13:53:36 +0700

    Prepare for testing

---
+++ CoditySolutions/Testing/Task3.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace CoditySolutions
+{
+    [TestFixture]
+    public class Task3
+    {
+        // ?/100
+        public int solution(int[] A, int K)
+        {
+            return 0;
+        }
+
+        [TestCase(new[] { 3, 8, 9, 7, 6 }, 3, new[] { 9, 7, 6, 3, 8 })]
+        public void Tests(int[] inputArray, int inputK, int[] expectedResult)
+        {
+            Assert.AreEqual(expectedResult, solution(inputArray, inputK));
+        }
+    }
+}
---
+++ CoditySolutions/Unknown/Equi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CoditySolutions
+{
+    /*
+     * Find an index in an array such that its prefix sum equals its suffix sum.
+     */
+
+    [TestFixture]
+    public class Equi
+    {
+        // 100/100
+        public int solution(int[] A)
+        {
+            if (A.Length == 0) return -1;
+
+            long sum = 0;
+            int i;
+
+            for (i = 0; i < A.Length; i++) sum += A[i];
+
+            long sum_left = 0;
+            for (i = 0; i < A.Length; i++)
+            {
+                long sum_right = sum - sum_left - A[i];
+                if (sum_left == sum_right) return i;
+                sum_left += A[i];
+            }
+
+            return -1;
+        }
+
+        [TestCase(new[] { -1, 3, -4, 5, 1, -6, 2, 1 }, 1)]
+        public void Tests(int[] inputArray, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, solution(inputArray));
+        }
+    }
+}
